cc6a95a25f4eb05d4e5601cb991a1184fd8df9e0
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-02-23 15:18:01 +0000

    [DEVOPS-643] Make sure all necessary packages are available in shell

--- default.nix
+++ default.nix
@@ -1,10 +1,17 @@
-with (import (fetchTarball https://github.com/NixOS/nixpkgs/archive/fb235c98d839ae37a639695ad088d19ef8382608.tar.gz) {});
 # NOTE: when bumping nixpkgs, also update nixpkgs-src.json and .travis.yml
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/61fbdb47a69f78998f55207e64122a0798047b5d.tar.gz) {} }:
+
+with pkgs;
 
 stdenv.mkDerivation {
   name = "daedalus";
 
-  buildInputs = [git python27 curl electron nodejs-6_x nodePackages.node-gyp nodePackages.node-pre-gyp ];
+  buildInputs = [
+    nix bash binutils coreutils curl
+    git python27 curl electron nodejs-6_x
+    nodePackages.node-gyp nodePackages.node-pre-gyp
+    gnumake
+  ];
 
   src = null;
 
